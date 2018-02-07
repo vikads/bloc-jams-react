57501f031190a77c1d7751b1911ead75dd788c60
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-07 10:58:38 -0800

    Adding navigation links, checkpoint_3_Routing_Bloc_Jams_React_completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'>Library</Link>
+          </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
