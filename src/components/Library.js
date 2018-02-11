8ca43788753ec29681b38be7d07538aa97944285
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-11 13:06:37 -0800

    Component State added

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: albumData };
+  }
+
   render () {
     return ()
       <section className="library">
