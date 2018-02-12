f199b63aad8cc4802136ae8edb3e9fbd1214d410
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-12 13:33:33 -0800

    Stting Album Data

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
+
+    this.state = {
+      album: album
+    };
+  }
+
   render() {
     return (
       <section className="album">
