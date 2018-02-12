37faa3d93cb340bb4900aa0cc8e7a2635d97f344
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-12 11:54:37 -0800

    Linking to Albums, checkpoint 5 completed

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Library extends Component {
   constructor(props) {
     super(props);
-    this.state = { albums: albumData };
+
+    this.state = {
+      albums: albumData
+    };
   }
 
   render () {
@@ -12,12 +16,12 @@ class Library extends Component {
       <section className="library">
         {
           this.state.albums.map ( (album, index) =>
-            <div key={index} >
+            <Link to={`/album/${album.slug}`} key={index} >
               <img src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
-            </div>
+            </Link>
           )
         }
       {/*We'll use the .map() array method to convert
