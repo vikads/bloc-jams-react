9f82d45109c19c7c1a8af7a8f193767f43a0bbf1
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-12 13:52:41 -0800

    Displaying Album Information

--- src/components/Album.js
+++ src/components/Album.js
@@ -17,7 +17,14 @@ class Album extends Component {
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album will go here
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
       </section>
     );
   }
