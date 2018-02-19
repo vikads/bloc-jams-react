b628d1c720366c775dc5cf3795912df725b4799b
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-19 10:10:32 -0800

    Pass states as props to PlayerBar

--- src/components/Album.js
+++ src/components/Album.js
@@ -101,6 +101,8 @@ class Album extends Component {
         <PlayerBar
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
+          currentTime={this.audioElement.currentTime}
+          durtion={this.audioElement.duration}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
