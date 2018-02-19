ed63070adf95163527140a07dbff860d94d3adec
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-19 12:18:44 -0800

    passed handleTimeChange(e) down as a Prop

--- src/components/Album.js
+++ src/components/Album.js
@@ -131,6 +131,7 @@ class Album extends Component {
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
         />
       </section>
     );
