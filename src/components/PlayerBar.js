6176a1f80e076cdf7611ffba814704328f1653b6
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-02-19 12:22:03 -0800

    onChange event listener added

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -24,7 +24,8 @@ class PlayerBar extends Component {
             value={(this.props.currentTime / this.props.duration) || 0}
             max="1"
             min="0"
-            step="0.01" 
+            step="0.01"
+            onChange={this.props.handleTimeChange}
           />
           <div className="total-time">{this.props.duration}</div>
         </section>
