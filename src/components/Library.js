10bcdfe851a5ee05efea4dc3b97e8f8033ac983b
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-03-06 15:01:23 -0800

    Library styling

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import '.././styles/Library.css';
+import { Row, Col, Image } from 'react-bootstrap';
 
 class Library extends Component {
   constructor(props) {
@@ -13,21 +15,25 @@ class Library extends Component {
 
   render () {
     return (
-      <section className="library">
+      <Row className="library show-grid">
         {
           this.state.albums.map ( (album, index) =>
-            <Link to={`/album/${album.slug}`} key={index} >
-              <img src={album.albumCover} alt={album.title} />
-              <div>{album.title}</div>
-              <div>{album.artist}</div>
-              <div>{album.songs.length} songs</div>
-            </Link>
+
+              <Col xs={12} sm={4} md={6}  key={index} className="libraryAlbumSection">
+                <Link to={`/album/${album.slug}`}  className="libraryAlbumInfo">
+                  <Image responsive src={album.albumCover} alt={album.title} />
+                  <div className="libraryAlbumTitle">{album.title}</div>
+                  <div className="libraryAlbumArtist">{album.artist}</div>
+                  <div className="libraryAlbumSongsLength">{album.songs.length} songs</div>
+                </Link>
+
+            </Col>
           )
         }
       {/*We'll use the .map() array method to convert
       the raw array data into an array of JSX elements
       that React can render. */}
-      </section>
+      </Row>
     );
   }
 }
